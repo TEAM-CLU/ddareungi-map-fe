989684fdeb44c669ecd8ac8c55c3391c4ab1c421
ssuzn <tnwls_723@naver.com>
2025-09-18 22:26:57 +0900

    feat: CalorieBadge 컴포넌트 제작 (#20)

--- src/app/screens/TestScreen.tsx
+++ src/app/screens/TestScreen.tsx
@@ -1,35 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { tw } from '@/shared/libs/tw-helper';
-import RoundButton from '@/shared/components/button/RoundButton';
-import SquareButton from '@/shared/components/button/SquareButton';
-import GenderButton from '@/shared/components/button/GenderButton';
+import CalorieBadge from '@/shared/components/badge/CalorieBadge';
 
 const TestScreen = () => {
   const [gender, setGender] = React.useState<'male' | 'female'>('male');
 
   return (
     <View style={tw('flex-1 items-center justify-center bg-surface-secondary')}>
-      <SquareButton title="스퀘어 버튼" onPress={() => {}} />
-      <View style={tw('h-4')} />
-      <RoundButton preset="lg" title="라운드 버튼" onPress={() => {}} />
-      <RoundButton preset="sm" title="인증하기" onPress={() => {}} />
-      <RoundButton preset="origin" title="출발" onPress={() => {}} />
-      <RoundButton preset="destination" title="도착" onPress={() => {}} />
-      <RoundButton preset="destination" title="반환점" onPress={() => {}} />
-      <View style={tw('h-4')} />
-      <View style={tw('flex-row')}>
-        <GenderButton
-          label="남성"
-          selected={gender === 'male'}
-          onPress={() => setGender('male')}
-        />
-        <GenderButton
-          label="여성"
-          selected={gender === 'female'}
-          onPress={() => setGender('female')}
-        />
-      </View>
+      <Text>Test Screen</Text>
+      <CalorieBadge value={143} />
     </View>
   );
 };
---
+++ src/shared/components/badge/CalorieBadge.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { tw } from '@/shared/libs/tw-helper';
+
+interface CalorieBadgeProps {
+  value: number;
+}
+
+const CalorieBadge = ({ value }: CalorieBadgeProps) => (
+  <View
+    style={[
+      tw('min-h-5 flex-row justify-center items-center bg-brand-primary'),
+      { minWidth: 70, borderRadius: 20 },
+    ]}
+  >
+    <Text
+      style={[
+        tw('text-on-surface-secondary leading-6'),
+        { fontFamily: 'Pretendard-Medium', fontSize: 10 },
+      ]}
+    >
+      {value}kcal
+    </Text>
+  </View>
+);
+
+export default CalorieBadge;
