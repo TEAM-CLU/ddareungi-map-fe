f8b0f16c4a938a89e4c27a1571bafb0fda9c1e22
ssuzn <tnwls_723@naver.com>
2025-10-02 00:13:42 +0900

    feat: 온보딩 페이지 제작 (#33)

--- src/app/screens/OnboardingScreen.tsx
+++ src/app/screens/OnboardingScreen.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { tw } from '@/shared/libs/tw-helper';
+import React, { useState } from 'react';
+import PagerView from 'react-native-pager-view';
+import OnboardingLayout from '@/features/onboarding/components/OnboardingLayout';
+import { ONBOARDING_DATA } from '@/features/onboarding/model/onboarding.constants';
 
 const OnboardingScreen = () => {
+  const [onBoardingStep, setOnBoardingStep] = useState<
+    'step1' | 'step2' | 'step3'
+  >('step1');
+
   return (
-    <View style={tw('flex-1 items-center justify-center bg-zinc-900')}>
-      <Text style={tw('text-yellow-400 text-2xl font-bold')}>Navigation</Text>
-    </View>
+    <PagerView
+      style={{ flex: 1 }}
+      initialPage={0}
+      onPageSelected={(event) => {
+        const { position } = event.nativeEvent;
+        setOnBoardingStep(`step${position + 1}` as 'step1' | 'step2' | 'step3');
+      }}
+    >
+      {ONBOARDING_DATA.map((item) => (
+        <OnboardingLayout
+          key={item.step}
+          step={item.step as 'step1' | 'step2' | 'step3'}
+          text1={item.text1}
+          text2={item.text2}
+          imageSource={item.imageSource}
+        />
+      ))}
+    </PagerView>
   );
 };
 
---
+++ src/features/onboarding/components/OnboardingLayout.tsx
@@ -0,0 +1,67 @@
+import StepIndicator from '@/shared/components/StepIndicator';
+import { Image, ImageStyle, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import LinearGradient from 'react-native-linear-gradient';
+import { tw } from '@/shared/libs/tw-helper';
+
+interface OnboardingLayoutProps {
+  step: 'step1' | 'step2' | 'step3';
+  text1: string;
+  text2: string;
+  imageSource: any;
+}
+
+const OnboardingLayout = ({
+  step,
+  text1,
+  text2,
+  imageSource,
+}: OnboardingLayoutProps) => {
+  return (
+    <SafeAreaView
+      style={tw('flex-1 bg-surface-primary relative')}
+      edges={['top', 'bottom']}
+    >
+      <LinearGradient
+        colors={['rgba(1,218,134,0)', '#01DA86']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+        style={tw('absolute bottom-0 w-full h-1/3')}
+      />
+
+      <View style={tw('flex-1 px-6')}>
+        <View style={tw('items-center pt-10')}>
+          <StepIndicator step={step} />
+        </View>
+
+        <View style={tw('h-[82px] items-center pt-12')}>
+          <Text
+            style={[
+              tw('font-primary-600 text-[#000] text-center'),
+              { fontSize: 20, lineHeight: 30 },
+            ]}
+          >
+            {text1}
+          </Text>
+          <Text
+            style={[
+              tw('font-primary-700 text-[#000] text-center'),
+              { fontSize: 20, lineHeight: 30 },
+            ]}
+          >
+            {text2}
+          </Text>
+        </View>
+      </View>
+
+      <View style={[tw('absolute w-full items-center'), { bottom: 50 }]}>
+        <Image
+          source={imageSource}
+          style={[tw('w-full'), { height: 522 }] as ImageStyle}
+          resizeMode="contain"
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+export default OnboardingLayout;
