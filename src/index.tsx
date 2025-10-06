df2c1c21f99a05d6cbb7822e0b8c3c339af92108
ssuzn <tnwls_723@naver.com>
2025-10-07 02:12:24 +0900

    feat: SlideModal 컴포넌트 구현 (#37)

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,29 @@ import App from './app/App';
 import { AuthProvider, TailwindProvider } from '@/app/providers';
 import { PaperProvider } from 'react-native-paper';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { tw } from './shared/libs/tw-helper';
 
 const Root = () => {
   const queryClient = new QueryClient();
 
   return (
-    <TailwindProvider>
-      <SafeAreaProvider>
-        <PaperProvider>
-          <AuthProvider>
-            <QueryClientProvider client={queryClient}>
-              <App />
-            </QueryClientProvider>
-          </AuthProvider>
-        </PaperProvider>
-      </SafeAreaProvider>
-    </TailwindProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <TailwindProvider>
+        <SafeAreaProvider>
+          <PaperProvider>
+            <AuthProvider>
+              <QueryClientProvider client={queryClient}>
+                <BottomSheetModalProvider>
+                  <App />
+                </BottomSheetModalProvider>
+              </QueryClientProvider>
+            </AuthProvider>
+          </PaperProvider>
+        </SafeAreaProvider>
+      </TailwindProvider>
+    </GestureHandlerRootView>
   );
 };
 
